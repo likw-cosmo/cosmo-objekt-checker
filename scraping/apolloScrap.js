b6eaf64e69d013e4658e7f9bd93e4f990c613134
likw-cosmo <likw.cosmo@protonmail.com>
2024-12-21 17:12:45 +0800

    feat: update auto change a if not data in z

--- scraping/apolloScrap.js
+++ scraping/apolloScrap.js
@@ -1,13 +1,26 @@
 module.exports.apolloScrap = async (slug) => {
+  let responseSlug = slug;
   const axios = require("axios");
-  const { data: objektInfoResponse } = await axios.get(
-    `https://apollo.cafe/api/objekts/by-slug/${slug}`
-  );
+  let objektInfoResponse;
+  try {
+    objektInfoResponse = await axios.get(
+      `https://apollo.cafe/api/objekts/by-slug/${responseSlug}`
+    );
+  } catch (error) {
+    // cut the last word in slug from z to a and try again
+    try {
+      responseSlug = slug.slice(0, -1) + "a";
+      const link = `https://apollo.cafe/api/objekts/by-slug/${responseSlug}`;
+      objektInfoResponse = await axios.get(link);
+    } catch (error) {
+      return null;
+    }
+  }
   const { data: objektMetaDataResponse } = await axios.get(
-    `https://apollo.cafe/api/objekts/metadata/${slug}`
+    `https://apollo.cafe/api/objekts/metadata/${responseSlug}`
   );
   return {
-    objektInfoResponse,
+    objektInfoResponse: objektInfoResponse.data,
     objektMetaDataResponse,
   };
 };
